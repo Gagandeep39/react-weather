abf4e09240c67acae0a9d951f237cae80d0ae361
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-31 22:49:17 +0530

    Created Hourly weather card

---
+++ src/components/WeatherDashboard/HourlyWeatherDisplay/HourlyWeatherCard/HourlyWeatherCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HourlyWeatherCard.css';
+import CloudyCardImage from '../../../../assets/cloudy.png';
+import RainyCardImage from '../../../../assets/rainy.png';
+import SunnyCardImage from '../../../../assets/sunny.png';
+
+const HourlyWeatherCard = (props) => {
+  return (
+    <div class='card'>
+      <p class='card-header'>11:00</p>
+      <img src={CloudyCardImage} alt='IMG NOT FOUND' />
+      <section class='card-body'>
+        <div class='card-title'>12°</div>
+        <div class='card-text'>Moderate Rain</div>
+      </section>
+    </div>
+  );
+};
+
+export default HourlyWeatherCard;
