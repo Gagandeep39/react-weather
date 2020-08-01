a823b3757454c28fde53529bc359bdec5267bfc5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-01 19:23:44 +0530

    Added Updateweather in reducer Added api in reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,31 @@
 const initialState = {
+  apiKey: process.env.REACT_APP_WEATHER_API_KEY,
   location: {
     lat: '19.1204672',
     long: '72.88698149999999',
+  },
+  weather: {
+    current: null,
+    daily: [],
+    hourly: [],
   },
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case 'STORE_LOCATION':
-      console.log(action.value);
       return {
         ...state,
-        location: action.value,
+        location: action.payload,
+      };
+    case 'UPDATE_WEATHER':
+      return {
+        ...state,
+        weather: {
+          current: action.payload.current,
+          hourly: action.payload.hourly,
+          daily: action.payload.daily,
+        },
       };
   }
   return state;
