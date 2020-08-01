2757785e7feed0494f7173c194006517e8a0c7f6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-01 12:27:52 +0530

    Added comonents to home screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import HourlyWeatherDisplay from './components/WeatherDashboard/HourlyWeatherDisplay/HourlyWeatherDisplay';
+import DailyWeatherDisplay from './components/WeatherDashboard/DailyWeatherDisplay/DailyWeatherDisplay';
+import Header from './components/Header/Header';
+import CurrentWeatherDisplay from './components/WeatherDashboard/CurrentWeatherDisplay/CurrentWeatherDisplay';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Header />
+      <CurrentWeatherDisplay />
+      <HourlyWeatherDisplay />
+      <DailyWeatherDisplay />
     </div>
   );
 }
