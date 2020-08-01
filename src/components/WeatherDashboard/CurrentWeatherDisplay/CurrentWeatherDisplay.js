acf30acd9d1b1d8d2ef310392848e015bc88793a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-01 19:25:24 +0530

    Implemented Current Weather Logic

--- src/components/WeatherDashboard/CurrentWeatherDisplay/CurrentWeatherDisplay.js
+++ src/components/WeatherDashboard/CurrentWeatherDisplay/CurrentWeatherDisplay.js
@@ -5,54 +5,116 @@ import RainyCardImage from '../../../assets/rainy.png';
 import SunnyCardImage from '../../../assets/sunny.png';
 import { ReactComponent as RefreshButton } from '../../../assets/refresh.svg';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class CurrentWeatherDisplay extends Component {
+  
   constructor(props) {
     super(props);
-    console.log(props);
     this.getLocation();
+    this.state = {
+      city: '',
+      lastUpdated: '',
+    };
   }
+
+  fetchAllDataFromServer = () => {
+    
+    const URL =
+      'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+      this.props.location.lat +
+      '&lon=' +
+      this.props.location.long +
+      '&units=metric&appid=' +
+      this.props.api;
+
+    axios.get(URL).then((response) => {
+      const payload = {
+        current: response.data.current,
+        hourly: response.data.hourly,
+        daily: response.data.daily,
+      };
+      this.setState({
+        lastUpdated: new Date().toLocaleTimeString(),
+      });
+      this.props.onUpdateWeather(payload);
+    });
+  };
 
   getLocation() {
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      const location = {
-        lat: position.coords.latitude,
-        long: position.coords.longitude,
-      };
-      this.props.onStoreLocation(location);
-    });
+    axios
+      .get(
+        `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_GEOLOCATION_API_KEY}`
+      )
+      .then((response) => {
+        this.setState({
+          city: response.data.city,
+        });
+        const location = {
+          lat: response.data.latitude,
+          long: response.data.longitude,
+        };
+        this.props.onStoreLocation(location);
+        this.fetchAllDataFromServer();
+      });
   }
 
-  refreshButtonHandler = () => {};
+  refreshButtonHandler = () => {
+    this.getLocation();
+  };
+
+  capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   render() {
-    return (
-      <div>
-        <div className={classes.card}>
-          <section class='card-header'>London</section>
-          <section className='current-title'>30°</section>
+    let weatherUI;
+    if (this.props.currentWeather) {
+      const currentWeather = this.props.currentWeather;
+      const description = this.capitalize(
+        currentWeather.weather[0].description
+      );
+      let weatherIcon = RainyCardImage;
+      if (currentWeather.weather[0].id === 800) weatherIcon = SunnyCardImage;
+      else if (currentWeather.weather[0].id <= 800)
+        weatherIcon = RainyCardImage;
+      else weatherIcon = CloudyCardImage;
 
-          <section class='box'>
-            <img src={SunnyCardImage} alt='IMG NOT FOUND' />
-            <div class='card-text'>Moderate Rain</div>
-          </section>
+      weatherUI = (
+        <div>
+          <div className={classes.card}>
+            <section class='card-header'>{this.state.city}</section>
+            <section className='current-title'>{currentWeather.temp}°</section>
 
-          <section class='card-body'>
-            <section class='card-text'>Updated as of 08:20</section>
-          </section>
-          <RefreshButton
-            onClick={this.refreshButtonHandler}
-            className='refresh-btn'
-          />
+            <section class='box'>
+              <img src={weatherIcon} alt='IMG NOT FOUND' />
+              <div class='card-text'>{description}</div>
+            </section>
+
+            <section class='card-body'>
+              <section class='card-text'>
+                Updated as of {this.state.lastUpdated}
+              </section>
+            </section>
+            <RefreshButton
+              onClick={this.refreshButtonHandler}
+              className='refresh-btn'
+            />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+
+    if (this.props.currentWeather) return weatherUI;
+    else return <div>Loading...</div>;
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     location: state.location,
+    api: state.apiKey,
+    currentWeather: state.weather.current,
   };
 };
 
@@ -62,7 +124,13 @@ const mapDispatchToProps = (dispatch) => {
     onStoreLocation: (location) => {
       dispatch({
         type: 'STORE_LOCATION',
-        value: location,
+        payload: location,
+      });
+    },
+    onUpdateWeather: (payload) => {
+      dispatch({
+        type: 'UPDATE_WEATHER',
+        payload: payload,
       });
     },
   };
