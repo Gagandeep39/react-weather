c3339e8642987c316bd2de6cdcfced0debc39e29
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-08-01 17:34:07 +0530

    Implemented redux

--- src/components/WeatherDashboard/CurrentWeatherDisplay/CurrentWeatherDisplay.js
+++ src/components/WeatherDashboard/CurrentWeatherDisplay/CurrentWeatherDisplay.js
@@ -1,28 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './CurrentWeatherDisplay.component.css';
 import CloudyCardImage from '../../../assets/cloudy.png';
 import RainyCardImage from '../../../assets/rainy.png';
 import SunnyCardImage from '../../../assets/sunny.png';
-import { ReactComponent as RefreshButton } from '../../../assets/refresh.svg'
+import { ReactComponent as RefreshButton } from '../../../assets/refresh.svg';
+import { connect } from 'react-redux';
 
-export default function CurrentWeatherDisplay() {
-  return (
-    <div>
-      <div className={classes.card}>
-      <section class='card-header'>London</section>
-      <section className='current-title'>30°</section>
+class CurrentWeatherDisplay extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.getLocation();
+  }
 
-      <section class="box">
-        <img src={SunnyCardImage} alt='IMG NOT FOUND' />
-        <div class='card-text'>Moderate Rain</div>
-      </section>
+  getLocation() {
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      const location = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      };
+      this.props.onStoreLocation(location);
+    });
+  }
 
-      <section class='card-body'>
-        <section class='card-text'>Updated as of 08:20</section>
-      </section>
-      {/* <hr style={{margin: '8px'}}/> */}
-      <RefreshButton className='refresh-btn'/>
-    </div>
-    </div>
-  );
+  refreshButtonHandler = () => {};
+
+  render() {
+    return (
+      <div>
+        <div className={classes.card}>
+          <section class='card-header'>London</section>
+          <section className='current-title'>30°</section>
+
+          <section class='box'>
+            <img src={SunnyCardImage} alt='IMG NOT FOUND' />
+            <div class='card-text'>Moderate Rain</div>
+          </section>
+
+          <section class='card-body'>
+            <section class='card-text'>Updated as of 08:20</section>
+          </section>
+          <RefreshButton
+            onClick={this.refreshButtonHandler}
+            className='refresh-btn'
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    location: state.location,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // Calling this will execute dispatch functin
+    onStoreLocation: (location) => {
+      dispatch({
+        type: 'STORE_LOCATION',
+        value: location,
+      });
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CurrentWeatherDisplay);
